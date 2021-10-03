cc0cfa5b87303ff7cd17a523f7ec854d0e9b5545
jared salgado <2345jared@gmail.com>
2021-10-03 17:47:09 -0500

    terminando el portafolio

---
+++ js/idioma.js
@@ -0,0 +1,18 @@
+export const idioma = {
+    ingles: {
+        about: "I am a front-end web developer specializing in cutting-edge technologies such as" +
+                " React.js, JavaScript, Sass, Css among others. Passionate to always learn new te" +
+                "gnologias and go deeper into those I have already learned.",
+        skills: "These are the technologies that I master and in which I hope to deepen further a" +
+                "nd share my knowledge."
+    },
+
+    español: {
+        about: "Soy desarrollador web front-end especializado en tecnologías de vanguardia como " +
+                "React.js, JavaScript, Sass, Css entre otras. Apasionado por siempre aprender nue" +
+                "vas tegnologias y profundizar mas en las que ya he aprendido.",
+
+        skills: "Estas son las tegnologias que domino y en las que espero profundizar mas y compa" +
+                "rtir mi conocimiento"
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,37 +1,26 @@
-const spans = document.querySelector("h1 span")
+const open1 = document.querySelector('.open1');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+const probar = document.querySelectorAll('#probar')
+import {idioma} from './idioma.js'
 
-spans.forEach(span => {
-    span.addEventListener('mouseover', e => {
-        span.classList.add('animated', 'rubberBand')
-    })
+
+
+
+
+
+
+
+
+document.addEventListener("click", e => {
+    if (e.target.matches(".open1")) {
+        modal_container.classList.add('show');
+        const titulo = document.getElementById("titulo").textContent = "DETS"
+        // const videoO = document.querySelector("video").src = "../video/simplescreenrecorder-2021-10-01_16.24.54.mp4"
+        e.preventDefault()
+    }
+})
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
 });
-
-spans.forEach(span => {
-    span.addEventListener('mouseout', e => {
-        span.classList.remove('animated', 'rubberBand')
-    })
-});
-
-
-const htmlBar = document.querySelector('.bar-html')
-const reactBar = document.querySelector('.bar-react')
-const javaBar = document.querySelector('.bar-javascript')
-const cssBar = document.querySelector('.bar-css')
-
-let t1 = new TimeLineLite()
-
-t1.fromTo(htmlBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(90% - 6px)`, ease: Power4.easeOut})
-    fromTo(reactBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(90% - 6px)`, ease: Power4.easeOut})
-    fromTo(javaBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(90% - 6px)`, ease: Power4.easeOut})
-    fromTo(cssBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(90% - 6px)`, ease: Power4.easeOut})
-
-
-const controller = new ScrollMagic.controller()
-
-const scene = new ScrollMagic.Scene({
-    triggerElemet: '.skills',
-    // triggerHook = 0
-}) 
-
-.setTween(t1)
-.addTo(controller)
